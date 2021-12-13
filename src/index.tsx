747ad0ce262e12924bd690acfa57b98e89312bc3
Jake Dennison <srt4rulez@gmail.com>
2021-12-12 21:24:42 -0500

    Add ChakraProvider to index, add theme with Poppins font

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.css';
 import App from 'App';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+import '@fontsource/poppins';
+import '@fontsource/poppins/700.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ChakraProvider
+            theme={theme}
+        >
+            <App />
+        </ChakraProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
