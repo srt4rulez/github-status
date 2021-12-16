338dae8dc38132a1afc58b2ad7597fae83bc7f7d
Jake Dennison <srt4rulez@gmail.com>
2021-12-15 20:08:12 -0500

    Add ComponentTileSkeleton component

---
+++ src/Components/ComponentTileSkeleton/ComponentTileSkeleton.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {
+    Skeleton,
+} from '@chakra-ui/react';
+
+const ComponentTileSkeleton = (): JSX.Element | null => {
+
+    return (
+
+        <Skeleton
+            width="100%"
+            height="200px"
+        />
+
+    );
+
+};
+
+export default ComponentTileSkeleton;
