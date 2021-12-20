e88b8c799c1ea2a17a4ffcbfa5e8afca2b217eca
Jake Dennison <srt4rulez@gmail.com>
2021-12-19 20:06:22 -0500

    Correct Summary updated_at field

--- src/types.ts
+++ src/types.ts
@@ -34,7 +34,9 @@ export interface GithubStatus {
 }
 
 export interface Summary {
-    updated_at?: string;
+    page: {
+        updated_at?: string;
+    };
     components?: Array<GithubComponent>;
     status?: GithubStatus;
 }
